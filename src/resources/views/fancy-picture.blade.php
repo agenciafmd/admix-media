c231ab3378d9a45437a878464225253685212205
Irineu Junior <irineu@fmd.ag>
2023-08-04 10:51:00 -0300

    feat(fancy-picture) adiciona aria-label e title

--- src/resources/views/fancy-picture.blade.php
+++ src/resources/views/fancy-picture.blade.php
@@ -1,5 +1,7 @@
 <a href="{{ $media->getUrl() }}"
    data-fancybox="{{ $dataFancybox }}"
    data-caption="{{ optional($media->getCustomProperty('meta'))[app()->getLocale()] ?? $media->name }}"
+   title="{{ optional($media->getCustomProperty('meta'))[app()->getLocale()] ?? $media->name }}"
+   aria-label="{{ optional($media->getCustomProperty('meta'))[app()->getLocale()] ?? $media->name }}"
    class="{{ $aClass }}">
     {!! $picture !!}</a>
